51662332c457e2fed1a056428557cc99082e957f
Victor Abreu <victorsabreu@gmail.com>
2018-02-17 17:04:13 -0200

    Exemplo de CRUD completo com WebAPI .Net

---
+++ WebApp/Controllers/ClienteController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        // GET: api/Cliente
+        public IEnumerable<Cliente> Get()
+        {
+            Cliente clientes = new Cliente();
+            return clientes.ListaClientes();
+        }
+
+        // GET: api/Cliente/5
+        public Cliente Get(int id)
+        {
+            Cliente clientes = new Cliente();
+
+            return clientes.ListaClientes().Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Cliente
+        public List<Cliente> Post([FromBody]Cliente cliente)
+        {
+            Cliente clientes = new Cliente();
+            clientes.Inserir(cliente);
+            return clientes.ListaClientes();
+        }
+
+        // PUT: api/Cliente/5
+        public List<Cliente> Put(int id, [FromBody]Cliente cliente)
+        {
+            Cliente clientes = new Cliente();
+            clientes.Atualizar(id, cliente);
+            return clientes.ListaClientes();
+        }
+
+        // DELETE: api/Cliente/5
+        public List<Cliente> Delete(int id)
+        {
+            Cliente clientes = new Cliente();
+            clientes.Remover(id);
+            return clientes.ListaClientes();
+        }
+    }
+}
---
+++ WebApp/Models/Cliente.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+
+namespace WebApp.Models
+{
+    public class Cliente
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int id, string nome, string sobrenome)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public List<Cliente> ListaClientes()
+        {
+            var path = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+
+            var json = File.ReadAllText(path);
+            var lista = JsonConvert.DeserializeObject<List<Cliente>>(json);
+            
+            return lista;
+        }
+
+        public bool Adicionar(List<Cliente> listaClientes)
+        {
+            var path = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+
+            var json = JsonConvert.SerializeObject(listaClientes, Formatting.Indented);
+
+            File.WriteAllText(path, json);
+
+            return true;
+        }
+        public Cliente Inserir(Cliente cliente)
+        {
+            var lista = this.ListaClientes();
+
+            var maxId = lista.Max(_cliente => _cliente.Id);
+
+            cliente.Id = maxId +1;
+
+            lista.Add(cliente);
+
+            Adicionar(lista);
+
+            return cliente;
+        }
+        public Cliente Atualizar(int id, Cliente cliente)
+        {
+            var lista = this.ListaClientes();
+
+            var index = lista.FindIndex(p => p.Id == id);
+            if (index >= 0)
+            {
+                cliente.Id = id;
+                lista[index] = cliente;
+            }
+            else
+            {
+                return null;
+            }
+
+            Adicionar(lista);
+            return cliente;
+        }
+        public bool Remover(int id)
+        {
+            var lista = this.ListaClientes();
+
+            var index = lista.FindIndex(p => p.Id == id);
+            if (index >= 0)
+            {
+                lista.RemoveAt(index);
+            }
+            else
+            {
+                return false;
+            }
+
+            Adicionar(lista);
+            return true;
+        }
+    }
+}
